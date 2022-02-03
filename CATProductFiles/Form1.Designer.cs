786312584092eab734fdefde43b23501eaa46657
Jesus Sobalvarro <jesus.sobalvarro@outlook.com>
2022-02-03 09:51:09 +0100

    Add project files.

---
+++ CATProductFiles/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace CATProductFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearchProduct = new System.Windows.Forms.Button();
+            this.txbPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.libList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnSearchProduct
+            // 
+            this.btnSearchProduct.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearchProduct.Location = new System.Drawing.Point(629, 51);
+            this.btnSearchProduct.Name = "btnSearchProduct";
+            this.btnSearchProduct.Size = new System.Drawing.Size(159, 23);
+            this.btnSearchProduct.TabIndex = 1;
+            this.btnSearchProduct.Text = "CAT Product...";
+            this.btnSearchProduct.UseVisualStyleBackColor = true;
+            this.btnSearchProduct.Click += new System.EventHandler(this.btnSearchProduct_Click);
+            // 
+            // txbPath
+            // 
+            this.txbPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbPath.Location = new System.Drawing.Point(12, 53);
+            this.txbPath.Name = "txbPath";
+            this.txbPath.ReadOnly = true;
+            this.txbPath.Size = new System.Drawing.Size(611, 20);
+            this.txbPath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "List of Files in the Product";
+            // 
+            // libList
+            // 
+            this.libList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.libList.FormattingEnabled = true;
+            this.libList.Location = new System.Drawing.Point(12, 80);
+            this.libList.Name = "libList";
+            this.libList.Size = new System.Drawing.Size(776, 355);
+            this.libList.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.libList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbPath);
+            this.Controls.Add(this.btnSearchProduct);
+            this.Name = "Form1";
+            this.Text = "CAT Product Files List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSearchProduct;
+        private System.Windows.Forms.TextBox txbPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox libList;
+    }
+}
+
---
+++ CATProductFiles/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CATProductFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearchProduct_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter= "CATProduct Files (*.CATProduct)|*.CATProduct";
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txbPath.Text = fileDialog.FileName;
+                List<string> productFiles = GetComponents(fileDialog.FileName);
+                libList.Items.Clear();
+                foreach (string file in productFiles)
+                {
+                    libList.Items.Add(file);
+                }
+            }
+        }
+
+        public static List<string> GetComponents(string productFullFileName)
+        {
+
+            //Declare the result list
+            List<string> result = new List<string>();
+
+            //Read the product file and store as an UTF8 string
+            string productUTFText = File.ReadAllText(productFullFileName, Encoding.UTF8);
+
+            //Find the beginning of the CATOctetArray, that means the start of the portion where the components are listed
+            int octetArrayStart = productUTFText.IndexOf("CATOctetArray");
+
+            //Find the end of the CATOctet Array
+            int octetArrayEnd = productUTFText.IndexOf("\u0008FINJPL", octetArrayStart);
+
+            //Calculate the length of the Octet Array
+            int octetArrayLenght = octetArrayEnd - octetArrayStart;
+
+            //Extract the portion of the OctetArray from the full text
+            string octetArray = productUTFText.Substring(octetArrayStart, octetArrayLenght);
+
+            //Clean up the Octet Array by eliminating the SOH Char
+            octetArray = octetArray.Replace(";\u0001", "");
+
+            //Create a raw list by splitting the octet array on "[SCH][EOT]File Keyword
+            string[] docIdseparator = { "\u0001\u0004File" };
+
+            List<string> rawFileList = octetArray.Split(docIdseparator, StringSplitOptions.None).ToList();
+
+            //Loop the list for further cleaning
+            foreach (string rawFile in rawFileList)
+            {
+                //If start with CATOctetArray ignore
+                if (rawFile.StartsWith("CATOctetArray")) { continue; }
+
+                //If the chars from 1 to 4 = feat then ignore.
+                if (rawFile.Substring(1).StartsWith("feat")) { continue; }
+
+                //Find the null char, then the char ", this is the beginning and the end of the raw file
+                int rawFileStartIndex = rawFile.IndexOf("\u0000");
+                int rawFileEndIndex = rawFile.IndexOf("\u0022");
+                int rawFileLenght = rawFileEndIndex - rawFileStartIndex;
+                string cleanFile = rawFile.Substring(rawFileStartIndex, rawFileLenght);
+
+                //Clean up by finding the last backslash and retrieving the string after it
+                int fileStartIndex = cleanFile.LastIndexOf("\u005c") + 1;
+                cleanFile = cleanFile.Substring(fileStartIndex);
+
+                //Clean up by eliminating null characters
+                cleanFile = cleanFile.Replace("\u0000", "");
+
+                //Clean up by eliminating last character
+                cleanFile = cleanFile.Substring(0, cleanFile.Length - 1);
+
+                result.Add(cleanFile);
+
+            }
+
+            return result;
+        }
+    }
+}
